021ff88cc6c65d87964a8f3c5ccb38a7d605371d
VitorGulicz <vdmgulicz@gmail.com>
2025-08-28 20:46:20 -0300

    att

--- Telas/cadastro_usuario.php
+++ Telas/cadastro_usuario.php
@@ -37,7 +37,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar Usuario</title>
     <link rel="stylesheet" href="../CSS/styles.css">
-    <script src="JS\mascara.js"></script>
+    <script src="../JS/mascara.js"></script>
     <link rel="stylesheet" href="../CSS/tabela.css">
 </head>
 <body>
